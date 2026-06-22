31e5885d6a71c76d53709a576088f8e541ae6336
sam <sam@foofrancine.copm>
2026-06-21 19:31:41 -0500

    modify for docker

--- src/managers/api.js
+++ src/managers/api.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:8088"
+export const API = "http://localhost:8000"
 
 export const authHeader = () => ({
   "Authorization": `Token ${localStorage.getItem("auth_token")}`,
